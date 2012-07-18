207fcc49b2944f8c37e418aa4616c76423d622a1
FWOM <fwom@hotmail.com>
2012-07-18 16:49:35 -0300

    Update master

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class Cliente < ActiveRecord::Base
 
-  validates_presence_of :nome, :message => " - deve ser preenchido"
+  validates_presence_of :nome, :message => " - deve ser preenchido seu puto"
   validates_uniqueness_of :nome, :message => " - cliente já cadastrado"
   validates_numericality_of :idade, :greater_than => 0, :less_than => 100, :message => " - deve ser um número entre 0 e 100"
 
